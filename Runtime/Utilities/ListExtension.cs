cdedcb56fa452a5d7a0307da5679dfa68921b14b
Niezoor <kamil.niezorawski@gmail.com>
2024-11-01 19:08:26 +0100

    ListExtention random for IEnumable extention

--- Runtime/Utilities/ListExtension.cs
+++ Runtime/Utilities/ListExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Utilities
 {
@@ -6,7 +7,14 @@ namespace Core.Utilities
     {
         public static T Random<T>(this List<T> list)
         {
+            var count = list.Count;
+            if (count == 0) return default;
             return list[UnityEngine.Random.Range(0, list.Count)];
+        }
+
+        public static T Random<T>(this IEnumerable<T> source)
+        {
+            return source.ToList().Random();
         }
     }
 }
