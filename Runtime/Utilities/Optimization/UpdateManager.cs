e8537d926db76d30b3d9759d58768005e1dcfed5
Niezoor <kamil.niezorawski@gmail.com>
2024-11-11 17:55:56 +0100

    UpdateManager

---
+++ Runtime/Utilities/Optimization/UpdateManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.Utilities.Optimization
+{
+    [DefaultExecutionOrder(-2999)] //After TimeCache
+    public class UpdateManager : Singleton<UpdateManager>
+    {
+        public static Action OnUpdate = () => { };
+        public static Action OnFixedUpdate = () => { };
+
+        private void Update()
+        {
+            OnUpdate.Invoke();
+        }
+
+        private void FixedUpdate()
+        {
+            OnFixedUpdate.Invoke();
+        }
+
+        private void OnDestroy()
+        {
+            OnUpdate = () => { };
+            OnFixedUpdate = () => { };
+        }
+    }
+}
