a499e3f3232d0a6a9da76c34cc428d8a2b315699
Niezoor <kamil.niezorawski@gmail.com>
2025-01-05 17:19:14 +0100

    Add Rotator

---
+++ Runtime/Utilities/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using Core.Utilities.Optimization;
+using UnityEngine;
+
+namespace Core.Utilities
+{
+    public class Rotator : MonoBehaviour
+    {
+        public Vector3 Speed;
+
+        private Transform t;
+
+        private void Awake()
+        {
+            t = transform;
+        }
+
+        private void Update()
+        {
+            t.Rotate(Speed * TimeCache.DeltaTime);
+        }
+    }
+}
