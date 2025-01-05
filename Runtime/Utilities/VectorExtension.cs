cc8d1a00b5c1fcd3c525797e975013ea06d13b31
Niezoor <kamil.niezorawski@gmail.com>
2025-01-05 16:31:00 +0100

    Update VectorExtension.cs

--- Runtime/Utilities/VectorExtension.cs
+++ Runtime/Utilities/VectorExtension.cs
@@ -11,18 +11,25 @@ namespace Core.Utilities
                    Mathf.Approximately(vec1.y, vec2.y) &&
                    Mathf.Approximately(vec1.z, vec2.z);
         }
-        
+
         public static bool Almost(this Vector2 vector2, Vector2 other, float tolerance) =>
             Math.Abs(vector2.x - other.x) < tolerance && Math.Abs(vector2.y - other.y) < tolerance;
 
-        public static Vector2 DirectionTo(this Vector2 from, Vector2 to)
+        public static Vector2 Direction(this Vector2 from, Vector2 to)
         {
             return to - from;
         }
 
-        public static Vector3 DirectionTo(this Vector3 from, Vector3 to)
+        public static Vector3 Direction(this Vector3 from, Vector3 to)
         {
             return to - from;
+        }
+
+        public static float DistanceSqr(this Vector2 a, Vector2 b)
+        {
+            float num1 = a.x - b.x;
+            float num2 = a.y - b.y;
+            return (float)((double)num1 * (double)num1 + (double)num2 * (double)num2);
         }
     }
 }
