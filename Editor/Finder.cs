5402380c522799e005ecd94d465987069f0a8b9f
Niezoor <kamil.niezorawski@gmail.com>
2025-01-05 17:19:02 +0100

    Update Finder.cs

--- Editor/Finder.cs
+++ Editor/Finder.cs
@@ -7,7 +7,9 @@ namespace Core.Editor
     {
         public static T FindAsset<T>(string name) where T : Object
         {
-            var guids = AssetDatabase.FindAssets($"t:{typeof(T)} {name}");
+            var searchString = $"t:{typeof(T).Name} {name}";
+            var guids = AssetDatabase.FindAssets(searchString);
+            Debug.Log($"Searching:{searchString}");
             if (guids.Length == 0) return null;
             var path = AssetDatabase.GUIDToAssetPath(guids[0]);
             var asset = AssetDatabase.LoadAssetAtPath<T>(path);
