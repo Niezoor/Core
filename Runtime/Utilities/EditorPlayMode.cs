f69f09e4be377e16c8ab75dce6e7f0f99c0cbba8
Niezoor <kamil.niezorawski@gmail.com>
2025-04-11 09:34:53 +0200

    EditorPlayMode current value

---
+++ Runtime/Utilities/EditorPlayMode.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Core.Utilities
+{
+    [InitializeOnLoad]
+    public static class EditorPlayMode
+    {
+        public static PlayModeStateChange CurrentPlayModeState { get; private set; }
+
+        static EditorPlayMode()
+        {
+            EditorApplication.playModeStateChanged -= ModeChanged;
+            EditorApplication.playModeStateChanged += ModeChanged;
+        }
+
+        static void ModeChanged(PlayModeStateChange playModeState)
+        {
+            CurrentPlayModeState = playModeState;
+        }
+    }
+}
+#endif
