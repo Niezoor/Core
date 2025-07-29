e8992fec1a9a524cb5e81b400d579ab62e6a5841
Niezoor <kamil.niezorawski@gmail.com>
2025-07-29 21:14:17 +0200

    Update BarReduceEffect.cs

--- Runtime/UI/Elements/BarReduceEffect.cs
+++ Runtime/UI/Elements/BarReduceEffect.cs
@@ -13,7 +13,9 @@ namespace Core.UI.Elements
         public float delay = 0.66f;
         public float duration = 0.33f;
         public Ease ease = Ease.Linear;
+        public bool debug;
 
+        private float prevValue;
         private Tween delayTween;
         private Tween animTween;
         private Coroutine coroutine;
@@ -41,16 +43,19 @@ namespace Core.UI.Elements
 
         private void UpdateValue()
         {
-            if (bar.Value < reduceFillImage.fillAmount)
+            if (bar.Value < reduceFillImage.fillAmount && prevValue > bar.Value)
             {
+                if (debug) Debug.Log($"Start reduce effect for {this}", this);
                 if (coroutine != null) StopCoroutine(coroutine);
-                coroutine = StartCoroutine(UpdateAnimatedCoroutine());
+                if (gameObject.activeInHierarchy) coroutine = StartCoroutine(UpdateAnimatedCoroutine());
                 //delayTween = DOVirtual.DelayedCall(delay, UpdateAnimated, false).OnKill(() => { delayTween = null; });
             }
             else
             {
                 reduceFillImage.fillAmount = bar.Value;
             }
+
+            prevValue = bar.Value;
         }
 
         private void UpdateAnimated()
@@ -62,15 +67,18 @@ namespace Core.UI.Elements
 
         private IEnumerator UpdateAnimatedCoroutine()
         {
+            if (debug) Debug.Log($"Start coroutine effect for {this}", this);
             yield return delaySeconds;
             var diff = reduceFillImage.fillAmount - bar.Value;
             var speed = diff / duration;
             while (reduceFillImage.fillAmount > bar.Value)
             {
+                //if (debug) Debug.Log($"Update coroutine effect for {this}", this);
                 reduceFillImage.fillAmount -= speed * TimeCache.deltaTime;
                 yield return waitForEndOfFrame;
             }
 
+            if (debug) Debug.Log($"End coroutine effect for {this}", this);
             reduceFillImage.fillAmount = bar.Value;
         }
     }
