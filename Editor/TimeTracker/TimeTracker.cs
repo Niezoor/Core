2361652442fab6c0aaa210606c3402a47a7985c7
Kamil <kamil.niezorawski@gmail.com>
2026-01-04 00:11:25 +0100

    TimeTracker time format changed to TotalHours

--- Editor/TimeTracker/TimeTracker.cs
+++ Editor/TimeTracker/TimeTracker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Sirenix.OdinInspector;
 using Sirenix.Utilities.Editor;
 using UnityEditor;
@@ -44,10 +45,9 @@ namespace Core.Editor.TimeTracker
             totalTimeStyle.focused.background = null;
             totalTimeStyle.alignment = TextAnchor.MiddleCenter;
             var totalTime = instance.TotalTime;
-            var time = totalTime;
-            var inText = time.Days > 0
-                ? totalTime.ToString(@"d\D\a\y\s\ hh\:mm\:ss")
-                : totalTime.ToString(@"hh\:mm\:ss");
+            //var time = totalTime;
+            //var inText = time.Days > 0 ? totalTime.ToString(@"d\D\a\y\s\ hh\:mm\:ss") : totalTime.ToString(@"hh\:mm\:ss");
+            var inText = $"{(int)totalTime.TotalHours}:{totalTime.Minutes:0#}:{totalTime.Seconds:0#}";
             EditorGUILayout.LabelField("Total time", totalTimeStyle);
             totalTimeStyle.fontSize = 24;
             totalTimeStyle.fontStyle = FontStyle.Bold;
@@ -71,10 +71,13 @@ namespace Core.Editor.TimeTracker
             {
                 try
                 {
-                    outText = outText.Replace("Days", ".");
-                    outText = outText.Replace(" ", "");
-                    Undo.RecordObject(instance, "Set time on time tracker");
-                    instance.TotalTime = TimeSpan.Parse(outText);
+                    //outText = outText.Replace("Days", ".");
+                    //outText = outText.Replace(" ", "");
+                    if (TryParseTotalHoursString(outText, out totalTime))
+                    {
+                        Undo.RecordObject(instance, "Set time on time tracker");
+                        instance.TotalTime = totalTime;
+                    }
                 }
                 catch (Exception e)
                 {
@@ -91,6 +94,33 @@ namespace Core.Editor.TimeTracker
         public void SaveSettings()
         {
             Save(true);
+        }
+
+        public static bool TryParseTotalHoursString(string value, out TimeSpan timeSpan)
+        {
+            timeSpan = TimeSpan.Zero;
+
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            string[] parts = value.Split(':');
+            if (parts.Length != 3)
+                return false;
+
+            if (!int.TryParse(parts[0], out int hours))
+                return false;
+
+            if (!int.TryParse(parts[1], out int minutes))
+                return false;
+
+            if (!int.TryParse(parts[2], out int seconds))
+                return false;
+
+            if (minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59)
+                return false;
+
+            timeSpan = new TimeSpan(hours, minutes, seconds);
+            return true;
         }
 
         [SettingsProvider]
